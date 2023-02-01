cab869fecf7185867c163af582a58f6ed18c3e6f
kchemorion <kchemorion@gmail.com>
2023-02-01 10:25:17 +0100

    Initial Layout

---
+++ supervisorGPT2/supervisorGPT2/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for supervisorGPT2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supervisorGPT2.settings')
+
+application = get_wsgi_application()
