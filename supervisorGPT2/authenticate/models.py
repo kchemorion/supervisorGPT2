2446fd5703717a755e4b7ed19aaf11ace712dc7e
kchemorion <kchemorion@gmail.com>
2023-02-02 03:25:11 +0100

    last stuipd copy

--- supervisorGPT2/authenticate/models.py
+++ supervisorGPT2/authenticate/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class QuestionType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    text = models.TextField()
+    response = models.TextField()
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    questions = models.ManyToManyField(Question, through='UserQuestion')
+
+    def __str__(self):
+        return self.username
+
+class UserQuestion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    date_answered = models.DateTimeField(auto_now_add=True)
